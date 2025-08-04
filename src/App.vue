eb6ab4fb4bca92de3d94b6f06b8424aca2f21e37
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-08-04 22:59:33 +0600

    feat: Update navbar link and text for drop simulation
    Updates the 'Ball Drop' link to 'Drop' and its associated route to '/drop-simulation'. Also adds a link to the homepage.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
 <template>
   <header class="text-gray-600 bg-white body-font fixed top-0 left-0 right-0 z-10">
     <div class="mx-auto flex flex-wrap items-center justify-between p-3 shadow rounded-[8px]">
-      <a class="flex title-font font-medium items-center text-gray-900">
+      <a class="flex title-font font-medium items-center text-gray-900" href="/">
         <span class="ml-3 text-xl font-bold">Physics Simulation</span>
       </a>
 
       <nav class="flex items-center text-base">
         <router-link to="/" class="mr-5 hover:text-gray-900">Home</router-link>
-        <router-link to="/ball-drop" class="mr-5 hover:text-gray-900">Ball Drop</router-link>
+        <router-link to="/drop-simulation" class="mr-5 hover:text-gray-900">Drop</router-link>
       </nav>
     </div>
   </header>
